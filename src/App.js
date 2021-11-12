4ee8b8b02b53dbac83bdb47e262067132c06f52a
Timothy Gonyea <timothy.gonyea@gmail.com>
2021-11-11 21:27:55 -0600

    Changing Image

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 import Header from './Header';
 
 function App() {
+  function changeImage(e) {
+    e.target.src = "https://prayer-sphere-resources.s3.us-east-2.amazonaws.com/images/logo2.png"
+  }
   return (
 
     <div className="App">
       <Header />
 
       <div className="App_body">
+        <div className="MainImage">
+          <img className="Main__Image" src="https://prayer-sphere-resources.s3.us-east-2.amazonaws.com/images/logo1.png" alt="" onMouseOver={changeImage} />
+
+
+        </div>
+
         <p>This is the app body</p>
 
       </div>
@@ -17,3 +26,4 @@ function App() {
 }
 
 export default App;
+
