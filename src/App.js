7f5de677fdc10782e08c5b7faa25f1e6f09420bc
Tim G <timothy.gonyea@gmail.com>
2022-06-15 17:04:21 -0500

    responsive columns

--- src/App.js
+++ src/App.js
@@ -13,45 +13,66 @@ function App() {
 
   return (
 
-    <div className="App">
+    <div class="App container-fluid">
 
 
       <Router>
-        <nav class="navbar navbar-expand-md bg-light sticky-top">
-          <div class="container-fluid">
+        <div class="row">
+          <div class="col">
+            <nav class="navbar navbar-expand-md bg-light sticky-top">
+              <div class="container-fluid">
 
-            <a class="navbar-brand" href="/">
-              <img class="logo d-inline-block align-text-top" src="https://prayer-sphere-resources.s3.us-east-2.amazonaws.com/images/logo2.png" alt='PrayerSphere Logo'></img>
-              Prayer Sphere
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls='navbarToggler' aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse ms-3" id="navbarToggler">
-              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <a class="navbar-brand" href="/">
+                  <img class="logo d-inline-block align-text-top" src="https://prayer-sphere-resources.s3.us-east-2.amazonaws.com/images/logo2.png" alt='PrayerSphere Logo'></img>
+                  Prayer Sphere
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls='navbarToggler' aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse ms-3" id="navbarToggler">
+                  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 
-                <Link to="/" class="nav-link" > Home</Link>
-                <Link to="/about" class="nav-link" > About</Link>
-                <Link to="/privacy" class="nav-link" > Privacy</Link>
+                    <Link to="/" class="nav-link" > Home</Link>
+                    <Link to="/about" class="nav-link" > About</Link>
+                    <Link to="/privacy" class="nav-link" > Privacy</Link>
 
-              </ul>
-            </div>
+                  </ul>
+                </div>
+              </div>
+
+            </nav>
+          </div>
+        </div>
+        <div class="row justify-content-md-center">
+          
+          <div class="col-sm-12 col-lg-10 mainSection">
+
+
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/privacy" element={<Privacy />} />
+              <Route path="*" element={<Error />} />
+
+            </Routes>
           </div>
 
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="*" element={<Error />} />
+         
+        </div>
 
-        </Routes>
+
+
+
+
+
       </Router>
+
+
 
       <div>
         <Footer />
       </div>
-    </div>
+    </div >
   );
 }
 
